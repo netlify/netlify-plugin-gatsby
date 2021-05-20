72006cdc1b8b1cd43408e532930af066aa18cadf
Luke Oliff <luke@lukeoliff.com>
2021-05-20 10:45:42 +0100

    feat: copy function wrappers to correct place on build

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 // @ts-check
 
 const path = require('path');
+const fs = require('fs-extra');
 
 const getCacheDirs = (PUBLISH_DIR) => [
   PUBLISH_DIR,
   path.normalize(`${PUBLISH_DIR}/../.cache`),
 ];
 
+const DEFAULT_FUNCTIONS_SRC = 'netlify/functions';
+
 module.exports = {
-  async onPreBuild({ netlifyConfig, constants, packageJson, inputs, utils }) {
+  async onPreBuild({ constants: { PUBLISH_DIR, FUNCTIONS_SRC = DEFAULT_FUNCTIONS_SRC }, utils }) {
     try {
       // print a helpful message if the publish dir is misconfigured
-      if (process.cwd() === constants.PUBLISH_DIR) {
+      if (process.cwd() === PUBLISH_DIR) {
         utils.build.failBuild(
-          `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
+          `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
         );
       }
 
-      const cacheDirs = getCacheDirs(constants.PUBLISH_DIR);
+      const cacheDirs = getCacheDirs(PUBLISH_DIR);
 
       if (await utils.cache.restore(cacheDirs)) {
         console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
@@ -25,53 +28,55 @@ module.exports = {
         console.log('No Gatsby cache found. Building fresh.');
       }
 
+      // warn if gatsby-plugin-netlify is missing
       const pluginName = 'gatsby-plugin-netlify';
       const gatsbyConfig = require(path.join(process.cwd(), 'gatsby-config'))
+  
       if (!gatsbyConfig.plugins.some((plugin) => (typeof plugin === 'string' ? plugin === pluginName : plugin.resolve === pluginName))) {
-        console.warn('Add gatsby-plugin-netlify to gatsby-config if you would like to support redirects.');
+        console.warn('Add `gatsby-plugin-netlify` to `gatsby-config` if you would like to support Gatsby redirects. 🎉');
       }
-
-      // add our functions to .gitignore
   
       // copying netlify wrapper functions into function dir
-  
-      // copy compiled gatsby functions into function dir
-  
+      await fs.copy(path.join(__dirname, 'templates/'), `${FUNCTIONS_SRC}/gatsby`, {
+        // overwrite: false,
+        // errorOnExist: true,
+        overwrite: true,
+      })
+
+      // add gatsby functions to .gitignore
+      var logStream = fs.createWriteStream(path.resolve('.gitignore'), { flags: 'a' });
+      logStream.write('# netlify-plugin-gatsby automated ignores\r\n');
+      logStream.end(`${FUNCTIONS_SRC}/gatsby\r\n`);
+
       // add redirect
   
     } catch (error) {
       // Report a user error
-      utils.build.failBuild('Error message', { error })
+      utils.build.failBuild('Error message', { error });
     }
 
     // Display success information
-    utils.status.show({ summary: 'Success!' })
+    utils.status.show({ summary: 'Success!' });
   },
 
-  // Other available event handlers
-  /*
-  // Before build commands are executed
-  onPreBuild() {},
-  // Build commands are executed
-  onBuild() {},
-  */
+  async onBuild({
+    netlifyConfig,
+    packageJson,
+    constants: { PUBLISH_DIR, FUNCTIONS_SRC = DEFAULT_FUNCTIONS_SRC },
+    utils,
+  }) {
+      // cp .cache/functions to netlify/functions/gatsby/functions
+
+  },
 
   // After Build commands are executed
-  async onPostBuild({ constants, utils }) {
-    const cacheDirs = getCacheDirs(constants.PUBLISH_DIR);
+  async onPostBuild({ constants: { PUBLISH_DIR }, utils }) {
+    const cacheDirs = getCacheDirs(PUBLISH_DIR);
 
     if (await utils.cache.save(cacheDirs)) {
-      console.log('Stored the Gatsby cache to speed up future builds.');
+      console.log('Stored the Gatsby cache to speed up future builds. 🔥');
     } else {
       console.log('No Gatsby build found.');
     }
   },
-  /*
-  // Runs on build success
-  onSuccess() {},
-  // Runs on build error
-  onError() {},
-  // Runs on build error or success
-  onEnd() {},
-  */
 }
