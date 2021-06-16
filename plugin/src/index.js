1264bf8bfc29202d74ee3cfd31966dc76b03b515
ehmicky <ehmicky@gmail.com>
2021-06-16 18:17:11 +0200

    chore: run ESLint/Prettier

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -37,105 +37,105 @@ const loadGatsbyFile = function (utils) {
 
 module.exports = {
   async onPreBuild({ constants: { PUBLISH_DIR }, utils, netlifyConfig }) {
-      // print a helpful message if the publish dir is misconfigured
-      if (process.cwd() === PUBLISH_DIR) {
-        utils.build.failBuild(
-          `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
-        )
-      }
+    // print a helpful message if the publish dir is misconfigured
+    if (process.cwd() === PUBLISH_DIR) {
+      utils.build.failBuild(
+        `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
+      )
+    }
 
-      const cacheDirs = getCacheDirs(PUBLISH_DIR)
+    const cacheDirs = getCacheDirs(PUBLISH_DIR)
 
-      if (await utils.cache.restore(cacheDirs)) {
-        console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️')
-      } else {
-        console.log('No Gatsby cache found. Building fresh.')
-      }
+    if (await utils.cache.restore(cacheDirs)) {
+      console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️')
+    } else {
+      console.log('No Gatsby cache found. Building fresh.')
+    }
 
-      // warn if gatsby-plugin-netlify is missing
-      const pluginName = 'gatsby-plugin-netlify'
-      const gatsbyConfig = loadGatsbyFile(utils)
+    // warn if gatsby-plugin-netlify is missing
+    const pluginName = 'gatsby-plugin-netlify'
+    const gatsbyConfig = loadGatsbyFile(utils)
 
-      if (!hasPlugin(gatsbyConfig.plugins, pluginName)) {
-        console.warn(
-          'Add `gatsby-plugin-netlify` to `gatsby-config.js` if you would like to support Gatsby redirects. 🎉',
-        )
-      }
+    if (!hasPlugin(gatsbyConfig.plugins, pluginName)) {
+      console.warn(
+        'Add `gatsby-plugin-netlify` to `gatsby-config.js` if you would like to support Gatsby redirects. 🎉',
+      )
+    }
 
-      if (hasPlugin(gatsbyConfig.plugins, 'gatsby-plugin-netlify-cache')) {
-        console.error(
-          "The plugin 'gatsby-plugin-netlify-cache' is not compatible with the Gatsby build plugin",
-        )
-        console.error(
-          'Please uninstall gatsby-plugin-netlify-cache and remove it from your gatsby-config.js',
-        )
-        utils.build.failBuild('Incompatible Gatsby plugin installed')
-      }
+    if (hasPlugin(gatsbyConfig.plugins, 'gatsby-plugin-netlify-cache')) {
+      console.error(
+        "The plugin 'gatsby-plugin-netlify-cache' is not compatible with the Gatsby build plugin",
+      )
+      console.error(
+        'Please uninstall gatsby-plugin-netlify-cache and remove it from your gatsby-config.js',
+      )
+      utils.build.failBuild('Incompatible Gatsby plugin installed')
+    }
 
-      if (
-        netlifyConfig.plugins.some(
-          (plugin) => plugin.package === 'netlify-plugin-gatsby-cache',
-        )
-      ) {
-        console.warn(
-          "The plugin 'netlify-plugin-gatsby-cache' is no longer required and should be removed.",
-        )
-      }
+    if (
+      netlifyConfig.plugins.some(
+        (plugin) => plugin.package === 'netlify-plugin-gatsby-cache',
+      )
+    ) {
+      console.warn(
+        "The plugin 'netlify-plugin-gatsby-cache' is no longer required and should be removed.",
+      )
+    }
   },
 
   async onBuild({
     constants: { PUBLISH_DIR, FUNCTIONS_SRC = DEFAULT_FUNCTIONS_SRC },
   }) {
-      // copying gatsby functions to functions directory
-      const compiledFunctions = path.join(
-        normalizedCacheDir(PUBLISH_DIR),
-        '/functions',
-      )
-      if (!fs.existsSync(compiledFunctions)) {
-        return
-      }
+    // copying gatsby functions to functions directory
+    const compiledFunctions = path.join(
+      normalizedCacheDir(PUBLISH_DIR),
+      '/functions',
+    )
+    if (!fs.existsSync(compiledFunctions)) {
+      return
+    }
 
-      // copying netlify wrapper functions into functions directory
-      await fs.copy(
-        path.join(__dirname, 'templates'),
-        path.join(FUNCTIONS_SRC, 'gatsby'),
-      )
+    // copying netlify wrapper functions into functions directory
+    await fs.copy(
+      path.join(__dirname, 'templates'),
+      path.join(FUNCTIONS_SRC, 'gatsby'),
+    )
 
-      await fs.copy(
-        compiledFunctions,
-        path.join(FUNCTIONS_SRC, 'gatsby', 'functions'),
-      )
+    await fs.copy(
+      compiledFunctions,
+      path.join(FUNCTIONS_SRC, 'gatsby', 'functions'),
+    )
 
-      const redirectsPath = path.resolve(`${PUBLISH_DIR}/_redirects`)
+    const redirectsPath = path.resolve(`${PUBLISH_DIR}/_redirects`)
 
-      await spliceConfig({
-        startMarker: '# @netlify/plugin-gatsby redirects start',
-        endMarker: '# @netlify/plugin-gatsby redirects end',
-        contents: '/api/* /.netlify/functions/gatsby 200',
-        fileName: redirectsPath,
-      })
+    await spliceConfig({
+      startMarker: '# @netlify/plugin-gatsby redirects start',
+      endMarker: '# @netlify/plugin-gatsby redirects end',
+      contents: '/api/* /.netlify/functions/gatsby 200',
+      fileName: redirectsPath,
+    })
 
-      // add gatsby functions to .gitignore if doesn't exist
-      const gitignorePath = path.resolve('.gitignore')
+    // add gatsby functions to .gitignore if doesn't exist
+    const gitignorePath = path.resolve('.gitignore')
 
-      await spliceConfig({
-        startMarker: '# @netlify/plugin-gatsby ignores start',
-        endMarker: '# @netlify/plugin-gatsby ignores end',
-        contents: `${FUNCTIONS_SRC}/gatsby`,
-        fileName: gitignorePath,
-      })
+    await spliceConfig({
+      startMarker: '# @netlify/plugin-gatsby ignores start',
+      endMarker: '# @netlify/plugin-gatsby ignores end',
+      contents: `${FUNCTIONS_SRC}/gatsby`,
+      fileName: gitignorePath,
+    })
   },
 
   async onPostBuild({ constants: { PUBLISH_DIR }, utils }) {
-      const cacheDirs = getCacheDirs(PUBLISH_DIR)
+    const cacheDirs = getCacheDirs(PUBLISH_DIR)
 
-      if (await utils.cache.save(cacheDirs)) {
-        utils.status.show({
-          title: 'Essential Gatsby Build Plugin ran successfully',
-          summary: 'Stored the Gatsby cache to speed up future builds. 🔥',
-        })
-      } else {
-        console.log('No Gatsby build found.')
-      }
+    if (await utils.cache.save(cacheDirs)) {
+      utils.status.show({
+        title: 'Essential Gatsby Build Plugin ran successfully',
+        summary: 'Stored the Gatsby cache to speed up future builds. 🔥',
+      })
+    } else {
+      console.log('No Gatsby build found.')
+    }
   },
 }
