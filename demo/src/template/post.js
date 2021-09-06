0a03df30b27239c57d1699f075dcc1e6a42395bd
Matt Kane <matt@gatsbyjs.com>
2021-09-06 08:12:33 +0100

    chore: remove extraneous import

--- demo/src/template/post.js
+++ demo/src/template/post.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import { Layout } from '../layout/default'
 import { containerCss } from './post.module.css'
 
