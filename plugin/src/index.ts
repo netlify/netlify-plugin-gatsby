8fff42b6aec2b959b980553da569ff470baa35c0
Matt Kane <m@mk.gg>
2021-09-06 10:27:12 +0100

    fix: externalise lmdb-store

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -146,7 +146,7 @@ The plugin no longer uses this and it should be deleted to avoid conflicts.\n`)
 
   netlifyConfig.functions['__api'] = {
     included_files: [path.posix.join(compiledFunctions, '**')],
-    external_node_modules: ['msgpackr-extract'],
+    external_node_modules: ['msgpackr-extract', 'lmdb-store'],
   }
 
   netlifyConfig.functions['__dsr'] = {
@@ -155,7 +155,7 @@ The plugin no longer uses this and it should be deleted to avoid conflicts.\n`)
       path.posix.join(CACHE_DIR, 'query-engine', '**'),
       path.posix.join(CACHE_DIR, 'page-ssr', '**'),
     ],
-    external_node_modules: ['msgpackr-extract'],
+    external_node_modules: ['msgpackr-extract', 'lmdb-store'],
   }
 
   await spliceConfig({
