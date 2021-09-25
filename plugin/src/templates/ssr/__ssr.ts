837f369e1988c831b53455db1bb241bd7f819881
Matt Kane <m@mk.gg>
2021-09-25 16:33:08 +0100

    feat: change how handler is created

--- plugin/src/templates/ssr/__ssr.ts
+++ plugin/src/templates/ssr/__ssr.ts
@@ -37,86 +37,88 @@ type PageSSR = {
   renderPageData: typeof renderPageDataType
 }
 
-prepareFilesystem()
-
-// Requiring this dynamically so esbuild doesn't re-bundle it
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { getData, renderHTML, renderPageData }: PageSSR = require(join(
-  CACHE_DIR,
-  'page-ssr',
-))
-
-// eslint-disable-next-line func-style, max-statements, complexity
-export const handler: Handler = async function handler(event) {
-  const eventPath = event.path
-  const isPageData =
-    eventPath.startsWith('/page-data/') && eventPath.endsWith('/page-data.json')
-
-  const pathName = isPageData
-    ? getPagePathFromPageDataPath(eventPath)
-    : eventPath
-
+function getHandler(): Handler {
+  prepareFilesystem()
+  // Requiring this dynamically so esbuild doesn't re-bundle it
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, node/global-require
+  const { getData, renderHTML, renderPageData }: PageSSR = require(join(
+    CACHE_DIR,
+    'page-ssr',
+  ))
   const graphqlEngine = getGraphQLEngine()
 
-  // Gatsby doesn't currently export this type. Hopefully fixed by v4 release
-  const page: IGatsbyPage & { mode?: string } =
-    graphqlEngine.findPageByPath(pathName)
+  // eslint-disable-next-line complexity
+  return async function handler(event) {
+    const eventPath = event.path
+    const isPageData =
+      eventPath.startsWith('/page-data/') &&
+      eventPath.endsWith('/page-data.json')
 
-  if (page?.mode !== `SSR`) {
-    const body = await readFile(
-      join(process.cwd(), 'public', '404.html'),
-      'utf8',
-    )
-    return {
-      statusCode: 404,
-      body,
-      headers: {
-        Tag: etag(body),
-        'Content-Type': 'text/html; charset=utf-8',
-        'X-Mode': 'SSR',
-      },
+    const pathName = isPageData
+      ? getPagePathFromPageDataPath(eventPath)
+      : eventPath
+
+    // Gatsby doesn't currently export this type. Hopefully fixed by v4 release
+    const page: IGatsbyPage & { mode?: string } =
+      graphqlEngine.findPageByPath(pathName)
+
+    if (page?.mode !== `SSR`) {
+      const body = await readFile(
+        join(process.cwd(), 'public', '404.html'),
+        'utf8',
+      )
+      return {
+        statusCode: 404,
+        body,
+        headers: {
+          Tag: etag(body),
+          'Content-Type': 'text/html; charset=utf-8',
+          'X-Mode': 'SSR',
+        },
+      }
     }
-  }
 
-  const req: SSRReq = {
-    query: event.queryStringParameters,
-    method: event.httpMethod,
-    url: event.path,
-    headers: event.headers,
-  }
+    const req: SSRReq = {
+      query: event.queryStringParameters,
+      method: event.httpMethod,
+      url: event.path,
+      headers: event.headers,
+    }
 
-  const data = await getData({
-    pathName,
-    graphqlEngine,
-    req,
-  })
+    const data = await getData({
+      pathName,
+      graphqlEngine,
+      req,
+    })
 
-  const headers = data.serverDataHeaders || {}
+    const headers = data.serverDataHeaders || {}
 
-  if (isPageData) {
-    const body = JSON.stringify(await renderPageData({ data }))
+    if (isPageData) {
+      const body = JSON.stringify(await renderPageData({ data }))
+      return {
+        statusCode: 200,
+        body,
+        headers: {
+          ETag: etag(body),
+          'Content-Type': 'application/json',
+          'X-Mode': 'SSR',
+          ...headers,
+        },
+      }
+    }
+
+    const body = await renderHTML({ data })
+
     return {
       statusCode: 200,
       body,
       headers: {
         ETag: etag(body),
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/html; charset=utf-8',
         'X-Mode': 'SSR',
         ...headers,
       },
     }
   }
-
-  const body = await renderHTML({ data })
-
-  return {
-    statusCode: 200,
-    body,
-    headers: {
-      ETag: etag(body),
-      'Content-Type': 'text/html; charset=utf-8',
-      'X-Mode': 'SSR',
-      ...headers,
-    },
-  }
 }
+export const handler: Handler = getHandler()
