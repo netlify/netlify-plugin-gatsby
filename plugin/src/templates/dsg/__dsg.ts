dea5daea980ba909aaae5f9f8da02887c65e9867
Matt Kane <matt.kane@netlify.com>
2021-09-27 13:35:51 +0100

    feat: change how handler is created (#127)
    * feat: change how handler is created
    
    * chore: upgrade

--- plugin/src/templates/dsg/__dsg.ts
+++ plugin/src/templates/dsg/__dsg.ts
@@ -1,5 +1,5 @@
 /**
- * Handler for DSR/DSG routes
+ * Handler for DSG routes
  */
 
 import { join } from 'path'
@@ -24,83 +24,85 @@ import {
   prepareFilesystem,
 } from './utils'
 
-// Doing this as an import side-effect as it only needs to happen once, when bootstrapping the function
-prepareFilesystem()
-
 type PageSSR = {
   getData: typeof getDataType
   renderHTML: typeof renderHTMLType
   renderPageData: typeof renderPageDataType
 }
 
-// Requiring this dynamically so esbuild doesn't re-bundle it
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { getData, renderHTML, renderPageData }: PageSSR = require(join(
-  CACHE_DIR,
-  'page-ssr',
-))
-
 const DATA_SUFFIX = '/page-data.json'
 const DATA_PREFIX = '/page-data/'
 
-export const handler: Handler = builder(async function handler(event) {
-  const eventPath = event.path
+function getHandler(): Handler {
+  prepareFilesystem()
+  // Requiring this dynamically so esbuild doesn't re-bundle it
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, node/global-require
+  const { getData, renderHTML, renderPageData }: PageSSR = require(join(
+    CACHE_DIR,
+    'page-ssr',
+  ))
 
-  const isPageData =
-    eventPath.endsWith(DATA_SUFFIX) && eventPath.startsWith(DATA_PREFIX)
-
-  const pathName = isPageData
-    ? getPagePathFromPageDataPath(eventPath)
-    : eventPath
   const graphqlEngine = getGraphQLEngine()
-  // Gatsby doesn't currently export this type. Hopefully fixed by v4 release
-  const page: IGatsbyPage & { mode?: string } =
-    graphqlEngine.findPageByPath(pathName)
 
-  // Is it DSR or DSG? One is the old name for the other.
-  if (!page?.mode?.startsWith('DS')) {
-    const body = await readFile(
-      join(process.cwd(), 'public', '404.html'),
-      'utf8',
-    )
+  return async function handler(event) {
+    const eventPath = event.path
 
-    return {
-      statusCode: 404,
-      body,
-      headers: {
-        Tag: etag(body),
-        'Content-Type': 'text/html; charset=utf-8',
-        'X-Mode': 'DSG',
-      },
+    const isPageData =
+      eventPath.endsWith(DATA_SUFFIX) && eventPath.startsWith(DATA_PREFIX)
+
+    const pathName = isPageData
+      ? getPagePathFromPageDataPath(eventPath)
+      : eventPath
+    // Gatsby doesn't currently export this type. Hopefully fixed by v4 release
+    const page: IGatsbyPage & { mode?: string } =
+      graphqlEngine.findPageByPath(pathName)
+
+    if (page?.mode !== 'DSG') {
+      const body = await readFile(
+        join(process.cwd(), 'public', '404.html'),
+        'utf8',
+      )
+
+      return {
+        statusCode: 404,
+        body,
+        headers: {
+          Tag: etag(body),
+          'Content-Type': 'text/html; charset=utf-8',
+          'X-Mode': 'DSG',
+        },
+      }
     }
-  }
-  const data = await getData({
-    pathName,
-    graphqlEngine,
-  })
+    const data = await getData({
+      pathName,
+      graphqlEngine,
+    })
 
-  if (isPageData) {
-    const body = JSON.stringify(await renderPageData({ data }))
+    if (isPageData) {
+      const body = JSON.stringify(await renderPageData({ data }))
+      return {
+        statusCode: 200,
+        body,
+        headers: {
+          ETag: etag(body),
+          'Content-Type': 'application/json',
+          'X-Mode': 'DSG',
+        },
+      }
+    }
+
+    const body = await renderHTML({ data })
+
     return {
       statusCode: 200,
       body,
       headers: {
         ETag: etag(body),
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/html; charset=utf-8',
         'X-Mode': 'DSG',
       },
     }
   }
+}
 
-  const body = await renderHTML({ data })
-
-  return {
-    statusCode: 200,
-    body,
-    headers: {
-      ETag: etag(body),
-      'Content-Type': 'text/html; charset=utf-8',
-      'X-Mode': 'DSG',
-    },
-  }
-})
+export const handler: Handler = builder(getHandler())
