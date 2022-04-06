07ecf960a7417630a92a3cb5e52c9ee1e3ef5e7d
Erica Pisani <pisani.erica@gmail.com>
2022-04-06 12:23:46 -0400

    ci: add --prefer-offline flag to npm installs of test

--- plugin/test/e2e-test.sh
+++ plugin/test/e2e-test.sh
@@ -4,7 +4,7 @@ set -e
 for dir in fixtures/*; do
     echo Running tests in "$dir"
     cd "$dir"
-    npm i --no-package-lock --legacy-peer-deps
+    npm i --no-package-lock --legacy-peer-deps --prefer-offline
     npm run test
     cd -
 done
