c9302ac02d434a3bf831411868045b980eabfebc
Erica Pisani <pisani.erica@gmail.com>
2022-05-18 13:30:44 -0400

    test: add coverage for onBuild method

--- plugin/test/unit/index.spec.ts
+++ plugin/test/unit/index.spec.ts
@@ -1,51 +1,224 @@
 import process from 'node:process'
 import { join } from 'path'
 
+import { NetlifyPluginRunUtilOptions, NetlifyPluginRunUtilResult } from '@netlify/build/types/options/netlify_plugin_run_util'
 import Chance from 'chance'
 
-import { onSuccess } from '../../src/index'
+import { onBuild, onSuccess } from '../../src/index'
 
 jest.mock('node-fetch', () => ({
   __esModule: true,
   default: jest.fn(),
 }))
 
+jest.mock('../../src/helpers/config', () => {
+  const configObj = jest.requireActual('../../src/helpers/config');
+
+  return {
+    ...configObj,
+    mutateConfig: jest.fn(),
+    createDatastoreMetadataFile: jest.fn(),
+    spliceConfig: jest.fn(),
+  }
+})
+
+jest.mock('../../src/helpers/functions', () => {
+  const functionsObj = jest.requireActual('../../src/helpers/functions');
+
+  return {
+    ...functionsObj,
+    writeFunctions: jest.fn(),
+  }
+})
+
+jest.mock('../../src/helpers/files', () => {
+  const filesObj = jest.requireActual('../../src/helpers/files');
+
+  return {
+    ...filesObj,
+    patchFile: jest.fn(),
+    relocateBinaries: jest.fn(),
+  }
+})
+
+
 const chance = new Chance()
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const mockFetchMethod = (url) => Promise.resolve()
+const constants = {
+  INTERNAL_FUNCTIONS_SRC: 'demo/.netlify/internal-functions',
+  PUBLISH_DIR: 'demo/public',
+  FUNCTIONS_DIST: 'demo/.netlify/functions',
+  EDGE_HANDLERS_DIST: 'demo/.netlify/edge-functions-dist/',
+  IS_LOCAL: true,
+  NETLIFY_BUILD_VERSION: '9000.0.0',
+  SITE_ID: chance.guid()
+}
+const netlifyConfig = {
+  build: {
+    command: 'npm run build',
+    publish: 'publish',
+    base: '.',
+    environment: {},
+    services: {},
+    processing: {
+      css: {},
+      js: {},
+      html: {},
+      images: {}
+    },
+  },
+  functions: { '*': {} },
+  redirects: [],
+  headers: [],
+  edge_handlers: [],
+  plugins: []
+}
 
-describe('onSuccess', () => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, node/global-require
-  const fetch = require('node-fetch').default
+const mockRun = function (file: string, args?: readonly string[], options?: NetlifyPluginRunUtilOptions): Promise<NetlifyPluginRunUtilResult> {
+  return Promise.resolve(void 0)
+}
+mockRun.command = function (command: string, options?: NetlifyPluginRunUtilOptions): Promise<NetlifyPluginRunUtilResult> {
+  return Promise.resolve(void 0)
+}
+const utils = {
+  build: {
+    failBuild(message) {
+      throw new Error(message)
+    },
+    failPlugin(message) {
+      throw new Error(message)
+    },
+    cancelBuild(message) {
+      throw new Error(message)
+    },
+  },
 
-  beforeEach(() => {
-    fetch.mockImplementation(mockFetchMethod)
-    process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN = 'true'
-    process.env.URL = chance.url()
-  })
+  run: mockRun,
+  cache: {
+    save: jest.fn(),
+    restore: jest.fn(),
+    list: jest.fn(),
+    remove: jest.fn(),
+    has: jest.fn()
+  },
+  status: {
+    show: jest.fn()
+  },
+  git: {
+    fileMatch: (globPattern: string) => [],
+    modifiedFiles: [],
+    createdFiles: [],
+    deletedFiles: [],
+    commits: [],
+    linesOfCode: () => Promise.resolve(chance.number())
+  },
+  functions: {
+    add: jest.fn(),
+    list: jest.fn(),
+    listAll: jest.fn()
+  }
+}
+
+const defaultArgs = {
+  netlifyConfig,
+  utils,
+  constants,
+  packageJson: {},
+  inputs: {},
+}
+
+describe('plugin', () => {
 
   afterEach(() => {
     jest.clearAllMocks()
     jest.resetAllMocks()
-    delete process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN
-    delete process.env.URL
   })
 
-  it('makes requests to pre-warm the lambdas if LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is enabled', async () => {
-    await onSuccess()
+  describe('onBuild', () => {
+    const { createDatastoreMetadataFile } = require('../../src/helpers/config')
 
-    expect(fetch).toHaveBeenNthCalledWith(
-      1,
-      join(process.env.URL, '.netlify/functions/__api'),
-    )
-    expect(fetch).toHaveBeenNthCalledWith(
-      2,
-      join(process.env.URL, '.netlify/functions/__dsg'),
-    )
-    expect(fetch).toHaveBeenNthCalledWith(
-      3,
-      join(process.env.URL, '.netlify/functions/__ssr'),
-    )
+    it('creates the metadata file for the Gatsby datastore when LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is enabled', async () => {
+      process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN = 'true'
+      createDatastoreMetadataFile.mockImplementation(() => Promise.resolve())
+
+      await onBuild(defaultArgs)
+
+      expect(createDatastoreMetadataFile).toHaveBeenCalled()
+      expect(createDatastoreMetadataFile).toHaveBeenCalledWith(constants.PUBLISH_DIR)
+
+      delete process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN
+    })
+
+    it('does not create the metadata file for the Gatsby datastore when LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is disabled', async () => {
+      process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN = 'false'
+      createDatastoreMetadataFile.mockImplementation(() => Promise.reject())
+
+      await onBuild(defaultArgs)
+
+      expect(createDatastoreMetadataFile).not.toHaveBeenCalled()
+
+      delete process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN
+    })
+
+    it('does not create the metadata file for the Gatsby datastore when LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is not defined', async () => {
+      createDatastoreMetadataFile.mockImplementation(() => Promise.reject())
+
+      await onBuild(defaultArgs)
+
+      expect(createDatastoreMetadataFile).not.toHaveBeenCalled()
+
+    })
+  })
+
+  describe('onSuccess', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, node/global-require
+    const fetch = require('node-fetch').default
+
+    beforeEach(() => {
+      fetch.mockImplementation(mockFetchMethod)
+      process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN = 'true'
+      process.env.URL = chance.url()
+    })
+
+    afterEach(() => {
+      delete process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN
+      delete process.env.URL
+    })
+
+    it('makes requests to pre-warm the lambdas if LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is enabled', async () => {
+      await onSuccess()
+
+      expect(fetch).toHaveBeenNthCalledWith(
+        1,
+        join(process.env.URL, '.netlify/functions/__api'),
+      )
+      expect(fetch).toHaveBeenNthCalledWith(
+        2,
+        join(process.env.URL, '.netlify/functions/__dsg'),
+      )
+      expect(fetch).toHaveBeenNthCalledWith(
+        3,
+        join(process.env.URL, '.netlify/functions/__ssr'),
+      )
+    })
+
+    it('does not make requests to pre-warm the lambdas if LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is disabled', async () => {
+      process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN = 'false'
+
+      await onSuccess()
+
+      expect(fetch).toBeCalledTimes(0)
+    })
+
+    it('does not make requests to pre-warm the lambdas if process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN is not defined', async () => {
+      delete process.env.LOAD_GATSBY_LMDB_DATASTORE_FROM_CDN
+
+      await onSuccess()
+
+      expect(fetch).toBeCalledTimes(0)
+    })
   })
 })
+
