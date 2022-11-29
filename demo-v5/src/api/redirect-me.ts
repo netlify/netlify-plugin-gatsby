ab96450f88e9a8361e26f2b5bb2b51287e591a17
Erica Pisani <pisani.erica@gmail.com>
2022-11-29 09:42:17 -0500

    chore: add demo site for Gatsby v5
    Include .nvmrc files in both demos to specify minimum node versions.

---
+++ demo-v5/src/api/redirect-me.ts
@@ -0,0 +1,8 @@
+import { GatsbyFunctionResponse, GatsbyFunctionRequest } from 'gatsby'
+
+export default function topLevel(
+  req: GatsbyFunctionRequest,
+  res: GatsbyFunctionResponse,
+) {
+  res.redirect(`/`)
+}
