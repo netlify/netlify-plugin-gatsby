f50c2a5525942db17976a136559e2096b08c4f3a
Erica Pisani <pisani.erica@gmail.com>
2023-01-12 14:43:34 -0500

    test: another test

--- demo-v5/netlify/functions/error.ts
+++ demo-v5/netlify/functions/error.ts
@@ -1,13 +1,32 @@
-import {AWSLambda} from '@sentry/serverless'
-import { HandlerEvent, HandlerContext } from "@netlify/functions";
+import {AWSLambda, NodeOptions} from '@sentry/serverless'
+import { wrap } from "@netlify/integrations";
+import { HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions";
 
-AWSLambda.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
+// Todo: Consider making this an accessible type through @netlify/integrations
+type FunctionHandler<Context = unknown> = (
+  event: HandlerEvent,
+  context: HandlerContext & Context,
+) => Promise<HandlerResponse>
 
+const withSentry = (
+  handler,
+  sentryConfiguration?: NodeOptions
+) => {
+  // Provide sensible defaults or allow user to override Sentry configuration
+  // when calling the method
+  AWSLambda.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    ...sentryConfiguration
+  });
+  
+  return AWSLambda.wrapHandler(handler) as FunctionHandler;
+};
 
-const handler = AWSLambda.wrapHandler(async (event: HandlerEvent, context: HandlerContext) => {
+const withIntegrations = wrap(withSentry) 
+
+const handler = 
+withIntegrations(async (event: HandlerEvent, context: HandlerContext) => {
   throw Error('this is an error')
 });
 
