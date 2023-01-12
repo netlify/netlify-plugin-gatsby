0bdca09cd0300892cd3c6d1dde923b272d4dd9d9
Erica Pisani <pisani.erica@gmail.com>
2023-01-12 13:57:21 -0500

    test: test

--- demo-v5/netlify/functions/error.ts
+++ demo-v5/netlify/functions/error.ts
@@ -1,13 +1,13 @@
-import Sentry from '@sentry/serverless'
+import {AWSLambda} from '@sentry/serverless'
 import { HandlerEvent, HandlerContext } from "@netlify/functions";
 
-Sentry.AWSLambda.init({
+AWSLambda.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
 });
 
 
-const handler = Sentry.AWSLambda.wrapHandler(async (event: HandlerEvent, context: HandlerContext) => {
+const handler = AWSLambda.wrapHandler(async (event: HandlerEvent, context: HandlerContext) => {
   throw Error('this is an error')
 });
 
--- demo-v5/netlify/functions/hello.ts
+++ demo-v5/netlify/functions/hello.ts
@@ -1,13 +1,13 @@
-import Sentry from '@sentry/serverless'
+import {AWSLambda} from '@sentry/serverless'
 import { HandlerEvent, HandlerContext } from "@netlify/functions";
 
-Sentry.AWSLambda.init({
+AWSLambda.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
 });
 
 
-const handler = Sentry.AWSLambda.wrapHandler(async (event: HandlerEvent, context: HandlerContext) => {
+const handler = AWSLambda.wrapHandler(async (event: HandlerEvent, context: HandlerContext) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
