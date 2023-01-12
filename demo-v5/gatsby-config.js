3ed17ef7cbd52d04e1ec7e1260e2d59f43a85468
Erica Pisani <pisani.erica@gmail.com>
2023-01-12 10:17:15 -0500

    test: add SENTRY_DSN env var to config

--- demo-v5/gatsby-config.js
+++ demo-v5/gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "@sentry/gatsby",
+      options: {
+        dsn: process.env.SENTRY_DSN,
+        sampleRate: 0.7,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/posts`,
