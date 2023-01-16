989589a8bfca69b1324568c68489338af1f9f32e
Erica Pisani <pisani.erica@gmail.com>
2023-01-16 09:09:29 -0500

    test: manually set the transaction name

--- demo-v5/netlify/functions/error.ts
+++ demo-v5/netlify/functions/error.ts
@@ -1,6 +1,7 @@
-import {AWSLambda, NodeOptions} from '@sentry/serverless'
+import {AWSLambda, NodeOptions, configureScope} from '@sentry/serverless'
 import { wrap } from "@netlify/integrations";
 import { HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions";
+import { basename } from 'path';
 
 // Todo: Consider making this an accessible type through @netlify/integrations
 type FunctionHandler<Context = unknown> = (
@@ -20,25 +21,6 @@ const withSentry = (
     ...sentryConfiguration
   });
 
-  // Throwing an error is the only way to get access to the stack trace
-  try{
-    throw Error()
-  } catch(err) {
-    // The string at index 2 is the function calling withSentry
-    const withSentryCallingFunction = err.stack.split('at ')[2].trim()
-    // Regex to pull the file name out of the string
-    const regex = /[^.](netlify\/functions\/.+[^\:\d])/g
-    const result = withSentryCallingFunction.match(regex)
-
-    if (result.length) {
-      const filename = result[0].split('/').pop()
-      console.log('FILENAME', filename)
-      // Configure the scope with the function file name so that transactions
-      // (in the Sentry performance context) can be grouped
-      AWSLambda.configureScope(scope => scope.setTransactionName(`netlify/functions/${filename}`));
-    }
-  }
-  
   return AWSLambda.wrapHandler(handler) as FunctionHandler;
 };
 
@@ -46,7 +28,8 @@ const withIntegrations = wrap(withSentry)
 
 const handler = 
 withIntegrations(async (event: HandlerEvent, context: HandlerContext) => {
-  throw Error('this is an error')
+  configureScope(scope => scope.setTransactionName(`netlify/functions/${basename(__filename)}`));  
+  throw Error('yet another error')
 });
 
 export { handler };
