729eeac788a28f96b08b805da3416f5c99936725
Erica Pisani <pisani.erica@gmail.com>
2023-01-25 14:47:38 -0500

    test: for science!

--- demo-v5/netlify/functions/test-fail-scheduled-function.ts
+++ demo-v5/netlify/functions/test-fail-scheduled-function.ts
@@ -1,29 +1,22 @@
+import { wrap } from "@netlify/integrations";
+import {SentryContext, withSentry} from '@netlify/sentry'
 import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
 
-// Todo: Can encapsulate this better
-const requestHeaders = new Headers()
-requestHeaders.set('Authorization', `DSN ${process.env.SENTRY_DSN}`)
-requestHeaders.set('Content-Type', "application/json")
-
-const startSentryCheckIn = async () => {
-  await fetch(`https://sentry.io/api/0/monitors/${process.env.SENTRY_CRON_MONITOR_FAILURE_ID}/checkins/`, {
-    method: "POST",
-    headers: requestHeaders,
-    body: JSON.stringify({status: "in_progress"})
-  })
-}
-
-const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  await startSentryCheckIn()
+const myHandler = async (event: HandlerEvent, context: HandlerContext & SentryContext) => {
   console.log("Received event:", event);
-
-  // By not adding a 'complete' heartbeat check to Sentry, this will be considered
-  // a failed run of the cron job
-  return {
-      statusCode: 200,
-  };
+  throw Error('error thrown by failed scheduled function')
 };
 
-const handler = schedule("@hourly", myHandler)
+const withIntegrations = wrap(withSentry)
+
+const config = {
+  sentry: {
+    enableCronMonitoring: true
+  }
+}
+
+const handlerWithIntegrations = withIntegrations(myHandler, config) as Handler
+
+const handler = schedule("@hourly", handlerWithIntegrations)
 
 export { handler };
