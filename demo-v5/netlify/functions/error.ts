1ccf6b25008bccfc65981c138e03bd391a78fd9f
Erica Pisani <pisani.erica@gmail.com>
2023-01-25 10:37:27 -0500

    test: test

--- demo-v5/netlify/functions/error.ts
+++ demo-v5/netlify/functions/error.ts
@@ -1,34 +1,66 @@
-import {AWSLambda, NodeOptions, configureScope} from '@sentry/serverless'
-import { wrap } from "@netlify/integrations";
-import { HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions";
+import Sentry, { AWSLambda, NodeOptions, captureException, captureMessage} from '@sentry/serverless'
+import { wrap, FunctionHandler, IntegrationHandler } from "@netlify/integrations";
+import { HandlerEvent, HandlerContext } from "@netlify/functions";
 import { basename } from 'path';
+// import {SentryContext, withSentry} from '@netlify/sentry'
 
-// Todo: Consider making this an accessible type through @netlify/integrations
-type FunctionHandler<Context = unknown> = (
-  event: HandlerEvent,
-  context: HandlerContext & Context,
-) => Promise<HandlerResponse>
+export type SentryConfig = {
+  sentry?: NodeOptions
+}
+export interface SentryContext {
+  sentry: {
+    captureException: typeof captureException
+    captureMessage: typeof captureMessage
+  };
+}
 
-const withSentry = (
+export const setDefaultTransactionName = (handler: FunctionHandler<SentryContext>) => {
+  const setTransactionNameMethod = `configureScope(scope => scope.setTransactionName("netlify/functions/${basename(__filename)}"))`;
+
+  const stringifiedHander = handler.toString()
+  const regex = /(async).+\{/g
+  const match = stringifiedHander.match(regex)
+  const modifiedHandlerCode = `
+  const configureScope = Sentry.configureScope;  
+  return ${match}\n
+    ${setTransactionNameMethod}\n
+  `
+  const modifiedHandler = new Function('Sentry', stringifiedHander.replace(regex, modifiedHandlerCode))
+  return modifiedHandler(Sentry)
+}
+
+export const withSentry: IntegrationHandler<SentryContext, SentryConfig> = (
   handler,
-  sentryConfiguration?: NodeOptions
+  config?,
 ) => {
-  // Provide sensible defaults or allow user to override Sentry configuration
-  // when calling the method
+  const sentry = {
+    captureException,
+    captureMessage,
+  }
+
+  const sentryConfigUserOverrides: NodeOptions = config?.sentry || {}
+  
   AWSLambda.init({
     dsn: process.env.SENTRY_DSN,
+    // This value means that all events are sent to Sentry. While
+    // this is not ideal for functions with higher numbers of invocations
+    // it will ensure that those just getting started will see their events
+    // be sent immediately to Sentry
     tracesSampleRate: 1.0,
-    ...sentryConfiguration
+    ...sentryConfigUserOverrides
   });
 
-  return AWSLambda.wrapHandler(handler) as FunctionHandler;
+  return AWSLambda.wrapHandler(async (event, context) => {
+    const updatedHandler = setDefaultTransactionName(handler)
+
+    return updatedHandler(event, { ...context, sentry });
+  }) as FunctionHandler;
 };
 
-const withIntegrations = wrap(withSentry) 
+const withIntegrations = wrap(withSentry)
 
 const handler = 
-withIntegrations(async (event: HandlerEvent, context: HandlerContext) => {
-  configureScope(scope => scope.setTransactionName(`netlify/functions/${basename(__filename)}`));  
+withIntegrations(async (event: HandlerEvent, context: HandlerContext & SentryContext) => {
   throw Error('yet another error')
 });
 
