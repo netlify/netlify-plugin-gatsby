0ba73677a98ba80d834cb53bc02788b1ecc61f9c
Erica Pisani <pisani.erica@gmail.com>
2023-01-25 15:11:33 -0500

    test: update success cron monitor

--- demo-v5/netlify/functions/test-success-scheduled-function.ts
+++ demo-v5/netlify/functions/test-success-scheduled-function.ts
@@ -1,41 +1,8 @@
+import { wrap } from "@netlify/integrations";
+import {SentryContext, withSentry} from '@netlify/sentry'
 import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
 
-// Todo: Can encapsulate this better
-const requestHeaders = new Headers()
-requestHeaders.set('Authorization', `DSN ${process.env.SENTRY_DSN}`)
-requestHeaders.set('Content-Type', "application/json")
-
-const startSentryCheckIn = async () => {
-  console.log('Starting Sentry check in')
-  await fetch(`https://sentry.io/api/0/monitors/${process.env.SENTRY_CRON_MONITOR_SUCCESS_ID}/checkins/`, {
-    method: "POST",
-    headers: requestHeaders,
-    body: JSON.stringify({status: "in_progress"})
-  })
-}
-
-const completeSentryCheckIn = async () => {
-  await fetch(`https://sentry.io/api/0/monitors/${process.env.SENTRY_CRON_MONITOR_SUCCESS_ID}/checkins/latest/`, {
-    method: "PUT",
-    headers: requestHeaders,
-    body: JSON.stringify({status: "ok"})
-  })
-  console.log('Sentry check in completed')
-}
-
-const withSentryHandler = (
-  handler,
-): Handler => {
-  return async (event: HandlerEvent, context: HandlerContext) => {
-    await startSentryCheckIn()  
-    const response = await handler(event, context)
-    await completeSentryCheckIn()
-    return response
-  }
-};
-
-
-const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+const myHandler = async (event: HandlerEvent, context: HandlerContext & SentryContext) => {
   console.log("Received event:", event);
   
   return {
@@ -43,6 +10,16 @@ const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) 
   };
 };
 
-const handler = schedule("@hourly", withSentryHandler(myHandler))
+const withIntegrations = wrap(withSentry)
+
+const config = {
+  sentry: {
+    enableCronMonitoring: true
+  }
+}
+
+const handlerWithIntegrations = withIntegrations(myHandler, config) as Handler
+
+const handler = schedule("@hourly", handlerWithIntegrations)
 
 export { handler };
