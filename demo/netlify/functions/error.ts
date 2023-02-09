3a68d9267cc8d791a7dd0c37203049960a19abfc
Erica Pisani <pisani.erica@gmail.com>
2023-02-09 14:49:28 -0500

    Forgot to save the file

---
+++ demo/netlify/functions/error.ts
@@ -0,0 +1,12 @@
+import { wrap } from "@netlify/integrations";
+import { HandlerEvent, HandlerContext } from "@netlify/functions";
+import {SentryContext, withSentry} from '@netlify/sentry'
+
+const withIntegrations = wrap(withSentry)
+
+const handler = 
+withIntegrations(async (event: HandlerEvent, context: HandlerContext & SentryContext) => {
+  throw Error('yet another error')
+});
+
+export { handler };
