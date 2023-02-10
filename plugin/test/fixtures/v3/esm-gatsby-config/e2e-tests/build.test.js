94f3549c12fec16ddfba1682cf9acd7c06de5039
pieh <misiek.piechowiak@gmail.com>
2023-02-10 12:09:48 +0100

    fix: don't fail entire build when gatsby-config fails to be imported

--- plugin/test/fixtures/v3/esm-gatsby-config/e2e-tests/build.test.js
+++ plugin/test/fixtures/v3/esm-gatsby-config/e2e-tests/build.test.js
@@ -6,5 +6,9 @@ describe('A site with "--require esm" and gatsby-config.js authored in ESM', () 
   it('successfully builds and warns that is unable to validate gatsby-config', async () => {
     const { success, logs } = await buildSite()
     expect(success).toBeTruthy()
+    expect(logs.stderr)
+      .toMatch(`Could not load gatsby-config.js: Cannot use import statement outside a module
+
+Unable to validate if 'gatsby-plugin-netlify' is setup correctly.`)
   })
 })
