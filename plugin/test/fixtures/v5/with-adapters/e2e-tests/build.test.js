36930000a58ab039afd82253d1ca4a8cfcf9343b
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-07-14 11:09:00 +0200

    test: check both stdout and stderr for logs

--- plugin/test/fixtures/v5/with-adapters/e2e-tests/build.test.js
+++ plugin/test/fixtures/v5/with-adapters/e2e-tests/build.test.js
@@ -7,14 +7,18 @@ describe('A site using gatsby version with adapters', () => {
   it('successfully builds and disables @netlify/plugin-gatsby and gatsby-plugin-netlify', async () => {
     const {
       success,
-      logs: { stdout },
+      logs: { stdout, stderr },
     } = await buildSite()
+
+    // in CI warnings are outputted to stderr (yikes)
+    const fullOutput = stdout + stderr
+
     expect(success).toBeTruthy()
 
-    expect(stdout).toContain(
+    expect(fullOutput).toContain(
       'Skipping @netlify/plugin-gatsby work, because used Gatsby version supports adapters.',
     )
-    expect(stdout).toContain('Disabling plugin "gatsby-plugin-netlify"')
+    expect(fullOutput).toContain('Disabling plugin "gatsby-plugin-netlify"')
 
     const _redirectsContent = readFileSync('public/_redirects', 'utf8')
 
